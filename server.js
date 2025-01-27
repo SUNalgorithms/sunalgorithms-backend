23a8dfe00debde28f6ba426e2cce1415367b29e4
SUNalgorithms <140968755+SUNalgorithms@users.noreply.github.com>
2025-01-27 16:15:50 +0200

    Initial backend commit

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const nodemailer = require('nodemailer');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://sunalgorithms.github.io', 'http://localhost:5173'],
+  methods: ['GET', 'POST'],
+  credentials: true
+}));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
